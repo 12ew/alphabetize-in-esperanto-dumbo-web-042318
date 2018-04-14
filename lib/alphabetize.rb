2eecba48cf1203c60f7860e2c58d52a0fb304514
github username <github email address>
2018-04-14 17:20:39 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars # makes arr of characters
+  # binding.pry
+  arr.sort_by do |sentence| #iterate over arr of sentences
+    sentence.chars.map do |character| alphabet.index(character) # iterate over
+      # each sentence and index ever letter
+      # binding.pry
+    end
+  end
 end
